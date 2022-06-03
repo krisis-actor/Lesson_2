c91d68ffcdd8b5b875cefbcff9ad8e3e4470ae0a
Kirill <kgrigoryev@icloud.com>
2022-06-03 09:11:12 +0300

    task_1_2

--- task_1.rb
+++ task_1.rb
@@ -1,1 +1,10 @@
-#Test
+# Хеш, содержащий месяцы и количество дней в месяце
+calendar = { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30,
+            July: 31, August: 31, September: 30, October: 31, November: 30,
+            December: 31}
+
+# Хэш с месяцами, у которых количество дней ровно 30
+selected_months = calendar.select {|key,value| value == 30}
+
+# Цикл вывода месяцев
+selected_months.each_key { |key| puts key.to_s }
