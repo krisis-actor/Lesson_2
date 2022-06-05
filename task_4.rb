6d0bf08c324da6157de67f3d7b42327568827d8c
Kirill <kgrigoryev@icloud.com>
2022-06-05 23:13:11 +0300

    task 4

--- task_4.rb
+++ task_4.rb
@@ -1,1 +1,14 @@
-#Test
+# Массив из букв алфавита
+alphabet = ('a'..'z').to_a
+
+# Массив из гласных букв
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+# Создание хэша-алфавита путем итерации массива с счетчиком i
+alphabet_hash = Hash.new
+i = 0
+alphabet.each { |x| alphabet_hash[x.to_sym] = i i += 1 }
+
+# Создание хэша-алфавита из гласных и вывод хэша
+vowels_hash = alphabet_hash.select { |key,value| vowels.include?(key.to_s) }
+puts vowels_hash
